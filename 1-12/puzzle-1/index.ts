a8bd4e3fbbdbd7a9be8f43c8846c29293441589c
alexandraseri <alexandras@wix.com>
2019-12-01 14:21:47 +0200

    done 1st puzzle of 1/12/2019

--- 1-12/puzzle-1/index.ts
+++ 1-12/puzzle-1/index.ts
@@ -1,3 +1,23 @@
 import * as fs from 'fs';
-import * as rd from 'readline'
+import * as path from 'path';
+import * as readline from 'readline';
 
+const rl = readline.createInterface({
+    input: fs.createReadStream(path.join(__dirname, "./input.txt"))
+});
+
+const moduleMasses: string[] = [];
+
+rl.on('line', (line: string) => moduleMasses.push(line));
+
+rl.on('close', () => {
+    let totalFuel = 0;
+    moduleMasses.forEach((massString: string) => {
+        const mass = parseInt(massString);
+        totalFuel += Math.floor(mass / 3) - 2;
+    });
+
+    console.log(totalFuel);
+})
+
+  
