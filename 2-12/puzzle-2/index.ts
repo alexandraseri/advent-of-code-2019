dfadff1630940b0d1685cf5c9ec3f731ef587f0b
alexandraseri <alexandras@wix.com>
2019-12-02 11:44:16 +0200

    solve puzzle 2

--- 2-12/puzzle-2/index.ts
+++ 2-12/puzzle-2/index.ts
@@ -6,13 +6,25 @@ const rl = readline.createInterface({
     input: fs.createReadStream(path.join(__dirname, "./input.txt"))
 });
 
-let ops: number[] = [];
+let ops: string;
 
-rl.on('line', (line: string) => ops = line.split(',').map(num => parseInt(num)));
+rl.on('line', (line: string) => ops = JSON.stringify(line.split(',').map(num => parseInt(num))));
 
 rl.on('close', () => {
-    ops[1] = 12;
-    ops[2] = 2;
+    for(let i = 0; i < 100; i++) {
+        for(let j = 0; j < 100; j++) {
+            let clearMemory: number[] = JSON.parse(ops);
+            clearMemory[1] = i;
+            clearMemory[2] = j;
+            const ops1 = calculateOps1(clearMemory);
+            if(ops1 === 19690720){
+                console.log(100 * i + j);
+            }
+        }
+    }    
+})
+
+const calculateOps1 = (ops) => {
     let step = 0;
     while(ops[step] !== 99 && ops.length > step) {
         switch(ops[step]) {
@@ -23,10 +35,8 @@ rl.on('close', () => {
                 ops[ops[step+3]] = ops[ops[step+1]] * ops[ops[step+2]]
                 break;
         }
-
         step += 4;
     }
-
-    console.log(ops[0])
-})
+    return ops[0]
+}
   
